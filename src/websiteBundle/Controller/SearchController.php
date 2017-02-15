f3d79a310a58a0b1cd16ddbc1ce85ce708b2e7c9
Sylvain Ripoll <ydrogen45@gmail.com>
2017-02-15 11:49:28 +0100

    Search first pass

--- src/websiteBundle/Controller/SearchController.php
+++ src/websiteBundle/Controller/SearchController.php
@@ -3,11 +3,24 @@
 namespace websiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use coreBundle\Entity\WebsiteStyxuserbase;
 
 class SearchController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('websiteBundle:search:search.html.twig');
+      $repositoryStyxuserbase = $this->getDoctrine()->getManager()->getRepository('coreBundle:WebsiteStyxuserbase');
+      $user = $repositoryStyxuserbase->findCOUCOUByName($request->request->get("result"))[0];
+
+      if($user) {
+        var_dump($user->getName());
+      } else {
+        var_dump(null);
+      }
+      // var_dump(@$user->getName());
+      // var_dump($request->request->get("result"));
+      exit;
+      return $this->render('websiteBundle:search:search.html.twig');
     }
 }
