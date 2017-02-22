3d855b4c14e2627fb915b8927837ee118d7e2cd2
Ziph0n <ziph0ntweak@gmail.com>
2017-02-22 09:33:55 +0100

    started working on event creation

---
+++ src/websiteBundle/Form/CreateEventDetailsFormType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace websiteBundle\Form;
+
+
+use coreBundle\Entity\PostPost;
+use coreBundle\Entity\PostReward;
+use coreBundle\Entity\WebsiteCategory;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateEventFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class,array('label'=>'Titre'))
+            ->add('category', EntityType::class, array(
+                'class' => WebsiteCategory::class,
+                'property' => 'nameCategory'
+            ))
+            ->add('description', TextareaType::class,array('label'=>'Description'));
+    }
+
+
+    public function getName()
+    {
+        return 'create_event';
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => PostPost::class,
+        ));
+    }
+//    /**
+//     * @param OptionsResolver $resolver
+//     */
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//        $resolver->setDefaults(array(
+//            'data_class' => 'websiteBundle\Entity\Styxuserbase'
+//        ));
+//    }
+}
