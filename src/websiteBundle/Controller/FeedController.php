02d4e5ec22cdf0ff30193cca1e546b5787ac7f02
Ziph0n <ziph0ntweak@gmail.com>
2017-03-06 10:33:20 +0100

    nb styxers (i know wrong branch blah blha blah :p)

--- src/websiteBundle/Controller/FeedController.php
+++ src/websiteBundle/Controller/FeedController.php
@@ -15,197 +15,200 @@ use websiteBundle\Form\CreatePostFormType;
 
 class FeedController extends Controller
 {
-  /**
-  * @param Request $request
-  * @return \Symfony\Component\HttpFoundation\Response
-  */
-  public function indexAction(Request $request)
-  {
-    $session = $this->container->get('session');
-    $sessionStarted = $this->container->get('session')->isStarted();
-    if(!$sessionStarted) {
-      $session->start();
-    }
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        $session = $this->container->get('session');
+        $sessionStarted = $this->container->get('session')->isStarted();
+        if(!$sessionStarted) {
+            $session->start();
+        }
 
-    $repositoryGroup = $this->getDoctrine()->getRepository('coreBundle:WebsiteGroup');
-    $repositoryPostPost = $this->getDoctrine()->getRepository('coreBundle:PostPost');
-    $repositoryStyxuserbase = $this->getDoctrine()->getRepository('coreBundle:WebsiteStyxuserbase');
-    $repositoryZone = $this->getDoctrine()->getRepository('coreBundle:WebsiteZone');
-    $repositoryStyxuserbaseZones = $this->getDoctrine()->getRepository('coreBundle:WebsiteStyxuserbaseZones');
-    $repositoryPostPostZones = $this->getDoctrine()->getRepository('coreBundle:PostPostZones');
-    $repositoryType = $this->getDoctrine()->getRepository('coreBundle:WebsiteType');
-    $repositoryReward = $this->getDoctrine()->getRepository('coreBundle:PostReward');
+        $repositoryGroup = $this->getDoctrine()->getRepository('coreBundle:WebsiteGroup');
+        $repositoryPostPost = $this->getDoctrine()->getRepository('coreBundle:PostPost');
+        $repositoryStyxuserbase = $this->getDoctrine()->getRepository('coreBundle:WebsiteStyxuserbase');
+        $repositoryZone = $this->getDoctrine()->getRepository('coreBundle:WebsiteZone');
+        $repositoryStyxuserbaseZones = $this->getDoctrine()->getRepository('coreBundle:WebsiteStyxuserbaseZones');
+        $repositoryPostPostZones = $this->getDoctrine()->getRepository('coreBundle:PostPostZones');
+        $repositoryType = $this->getDoctrine()->getRepository('coreBundle:WebsiteType');
+        $repositoryReward = $this->getDoctrine()->getRepository('coreBundle:PostReward');
 
-    $types = $repositoryType->findAll();
-    $rewards = $repositoryReward->findAll();
+        $types = $repositoryType->findAll();
+        $rewards = $repositoryReward->findAll();
+        $users = $repositoryStyxuserbase->findAll();
+        $users = count($users);
 
-    $idUser = $this->getUser()->getId();
-    $user = $repositoryStyxuserbase->findById($idUser)[0];
-    $user_zone = $repositoryStyxuserbaseZones->findByStyxuserbase($user->getId())[0];
+        $idUser = $this->getUser()->getId();
+        $user = $repositoryStyxuserbase->findById($idUser)[0];
+        $user_zone = $repositoryStyxuserbaseZones->findByStyxuserbase($user->getId())[0];
 
-    if ($repositoryGroup->findById($user->getGroup()->getId())[0]->getName() == 'student') {
-      if($user_zone->getZone() != NULL) {
-        $name_zone = $user_zone->getZone()->getName();  # Nom de la ville de l'utilisateur
-      } else {
-        $name_zone = 'Orléans';
-      }
-    } else {
-      $name_zone = 'Orléans';
-    }
-    $filtres = [$name_zone];
-
-
-    if($session->get('newsfeed_filter') != NULL) {
-      $filtre = $session->get('newsfeed_filter');
-    } else {
-      $filtre = 0;
-    }
-
-    if(array_key_exists('filter', $request->query->all())) {
-      $filtre = $request->query->get('filter');
-      $session->set('newsfeed_filter', (Integer)$filtre-1);
-      if(array_key_exists('zone', $request->query->all())) {
-        $zone = $request->query->get('zone');
-        $session->set('zone_filter', $zone);
-      }
-      header("Location: /feed");
-      exit;
-    }
-
-    if($user_zone->getZone() != NULL) {
-      $zone = $user_zone->getZone();
-    } else {
-      // $zone = $repositoryZone->findById(1)[0];
-    }
-
-    $posts = null;
-
-    if($filtre == 0) {
-      $posts = $repositoryPostPostZones->findByZone($zone->getId());
-      if($session->get('zone_filter') != NULL) {
-        $zone_filter = $session->get('zone_filter');
-      } else {
-        $zone_filter = 1;
-      }
-    }
-
-    if($filtre == 1) {
-      if($session->get('zone_filter') != NULL) {
-        $zone_filter = $session->get('zone_filter');
-      } else {
-        $zone_filter = 0;
-      }
-      $posts = $repositoryPostPostZones->findByZone($zone_filter);
-      if($repositoryZone->findById($zone_filter)[0] != NULL) {
-        $zone = $repositoryZone->findById($zone_filter)[0];
-      } else {
-        $zone = $repositoryZone->findById(1)[0];
-      }
-      $name_zone = $zone->getName();
-    }
-
-    $filtre = $session->get('newsfeed_filter');
-    if(!$filtre) {
-      $filtre = 0;
-    }
-    $zone = $session->get('zone_filter');
-    if(!$zone) {
-      $zone = 1;
-    }
-    $zone = $repositoryZone->findById($zone)[0];
+        if ($repositoryGroup->findById($user->getGroup()->getId())[0]->getName() == 'student') {
+            if($user_zone->getZone() != NULL) {
+                $name_zone = $user_zone->getZone()->getName();  # Nom de la ville de l'utilisateur
+            } else {
+                $name_zone = 'Orléans';
+            }
+        } else {
+            $name_zone = 'Orléans';
+        }
+        $filtres = [$name_zone];
 
 
+        if($session->get('newsfeed_filter') != NULL) {
+            $filtre = $session->get('newsfeed_filter');
+        } else {
+            $filtre = 0;
+        }
 
-    $i = 1;
-    while($repositoryPostPost->findById($i) != null) {
-      $i++;
-    }
+        if(array_key_exists('filter', $request->query->all())) {
+            $filtre = $request->query->get('filter');
+            $session->set('newsfeed_filter', (Integer)$filtre-1);
+            if(array_key_exists('zone', $request->query->all())) {
+                $zone = $request->query->get('zone');
+                $session->set('zone_filter', $zone);
+            }
+            header("Location: /feed");
+            exit;
+        }
 
-    // var_dump($zone->getId());
+        if($user_zone->getZone() != NULL) {
+            $zone = $user_zone->getZone();
+        } else {
+            // $zone = $repositoryZone->findById(1)[0];
+        }
 
-    $em = $this->getDoctrine()->getManager();
-    $query = $em->createQuery('
+        $posts = null;
+
+        if($filtre == 0) {
+            $posts = $repositoryPostPostZones->findByZone($zone->getId());
+            if($session->get('zone_filter') != NULL) {
+                $zone_filter = $session->get('zone_filter');
+            } else {
+                $zone_filter = 1;
+            }
+        }
+
+        if($filtre == 1) {
+            if($session->get('zone_filter') != NULL) {
+                $zone_filter = $session->get('zone_filter');
+            } else {
+                $zone_filter = 0;
+            }
+            $posts = $repositoryPostPostZones->findByZone($zone_filter);
+            if($repositoryZone->findById($zone_filter)[0] != NULL) {
+                $zone = $repositoryZone->findById($zone_filter)[0];
+            } else {
+                $zone = $repositoryZone->findById(1)[0];
+            }
+            $name_zone = $zone->getName();
+        }
+
+        $filtre = $session->get('newsfeed_filter');
+        if(!$filtre) {
+            $filtre = 0;
+        }
+        $zone = $session->get('zone_filter');
+        if(!$zone) {
+            $zone = 1;
+        }
+        $zone = $repositoryZone->findById($zone)[0];
+
+
+
+        $i = 1;
+        while($repositoryPostPost->findById($i) != null) {
+            $i++;
+        }
+
+        // var_dump($zone->getId());
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('
     SELECT pp
     FROM coreBundle:PostPost pp
     WHERE pp.zone = :zone
     ')
-    ->setParameter('zone', $zone);
-    $posts = null;
-    $posts = $query->getResult();
-    for ($i=0; $i < sizeof($posts); $i++) {
-      $posts[$i] = array('post' => $posts[$i]);
+            ->setParameter('zone', $zone);
+        $posts = null;
+        $posts = $query->getResult();
+        for ($i=0; $i < sizeof($posts); $i++) {
+            $posts[$i] = array('post' => $posts[$i]);
+        }
+
+        $futur_event = $repositoryPostPostZones->findBy(array('zone'=>$zone));
+        $def_posts = [];
+        for ($i=0; $i < sizeof($futur_event); $i++) {
+            $futur_event_posts[$i] = $futur_event[$i]->getPost();
+            if(!$futur_event_posts[$i]->getDeleted()) {
+                $def_posts[] = $futur_event_posts[$i];
+            }
+        }
+        usort($def_posts, function($a, $b) {
+            $a_current_date = $a->getPostponedAt() ?: $a->getCreatedAt();
+            $b_current_date = $b->getPostponedAt() ?: $b->getCreatedAt();
+            if($a_current_date == $b_current_date) {
+                return 0;
+            } else if($a_current_date > $b_current_date) {
+                return 1;
+            } else if($a_current_date < $b_current_date) {
+                return -1;
+            }
+        });
+        $futur_event = null;
+        for ($i=0; $i < 6; $i++) {
+            if(!empty($def_posts[$i])) {
+                $futur_event[] = $def_posts[$i];
+            }
+        }
+
+        //var_dump($futur_event);
+
+        // var_dump($posts);
+
+        $ville = new WebsiteZone();
+        $cityForm = $this->createForm(new CityFormType(), $ville);
+        if ($cityForm->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ville);
+            $em->flush();
+        }
+
+        $repositoryReward = $this->getDoctrine()->getRepository('coreBundle:PostReward');
+        $rewards = $repositoryReward->findAll();
+        // $rewards = ['rewardsss' => $rewards];
+        // var_dump($rewards);
+        // exit;
+
+        $postpost = new PostPost();
+        $postForm = $this->createForm(new CreatePostFormType(), $postpost);
+        if ($postForm->handleRequest($request)->isValid()) {
+            $postpost->setOwner($this->getUser());
+            $postpost->setZone($zone);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($postpost);
+            $em->flush();
+            $postid = $postpost->getId();
+        }
+
+        // exit;
+        return $this->render('@website/feed/feed.html.twig', array(
+            'cityForm' => $cityForm->createView(),
+            'postForm' => $postForm->createView(),
+            'posts' => $posts,
+            'futur_event' => $futur_event,
+            'types' => $types,
+            'rewards' => $rewards,
+            'users' => $users,
+            'selected' => strval($zone->getId()),
+        ));
     }
 
-    $futur_event = $repositoryPostPostZones->findBy(array('zone'=>$zone));
-    $def_posts = [];
-    for ($i=0; $i < sizeof($futur_event); $i++) {
-      $futur_event_posts[$i] = $futur_event[$i]->getPost();
-      if(!$futur_event_posts[$i]->getDeleted()) {
-        $def_posts[] = $futur_event_posts[$i];
-      }
-    }
-    usort($def_posts, function($a, $b) {
-      $a_current_date = $a->getPostponedAt() ?: $a->getCreatedAt();
-      $b_current_date = $b->getPostponedAt() ?: $b->getCreatedAt();
-      if($a_current_date == $b_current_date) {
-        return 0;
-      } else if($a_current_date > $b_current_date) {
-        return 1;
-      } else if($a_current_date < $b_current_date) {
-        return -1;
-      }
-    });
-    $futur_event = null;
-    for ($i=0; $i < 6; $i++) {
-      if(!empty($def_posts[$i])) {
-        $futur_event[] = $def_posts[$i];
-      }
-    }
-
-    //var_dump($futur_event);
-
-    // var_dump($posts);
-
-    $ville = new WebsiteZone();
-    $cityForm = $this->createForm(new CityFormType(), $ville);
-    if ($cityForm->handleRequest($request)->isValid()) {
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($ville);
-      $em->flush();
-    }
-
-    $repositoryReward = $this->getDoctrine()->getRepository('coreBundle:PostReward');
-    $rewards = $repositoryReward->findAll();
-    // $rewards = ['rewardsss' => $rewards];
-    // var_dump($rewards);
-    // exit;
-
-    $postpost = new PostPost();
-    $postForm = $this->createForm(new CreatePostFormType(), $postpost);
-    if ($postForm->handleRequest($request)->isValid()) {
-      $postpost->setOwner($this->getUser());
-      $postpost->setZone($zone);
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($postpost);
-      $em->flush();
-      $postid = $postpost->getId();
-    }
-
-    // exit;
-    return $this->render('@website/feed/feed.html.twig', array(
-      'cityForm' => $cityForm->createView(),
-      'postForm' => $postForm->createView(),
-      'posts' => $posts,
-      'futur_event' => $futur_event,
-      'types' => $types,
-      'rewards' => $rewards,
-      'selected' => strval($zone->getId()),
-    ));
-  }
-
-  //     public function connected_user_count() {
-  //       $since_day = timezone.now() - timedelta(days=3);
-  //       $since_day.strftime('%m%d%y');
-  //       return len(StyxUserBase.objects.filter(last_login__gt=since_day));
+    //     public function connected_user_count() {
+    //       $since_day = timezone.now() - timedelta(days=3);
+    //       $since_day.strftime('%m%d%y');
+    //       return len(StyxUserBase.objects.filter(last_login__gt=since_day));
 
 }
