8227335017d35f4ddd2b4facdb7276d1543bf3af
Sylvain Ripoll <ydrogen45@gmail.com>
2017-03-13 17:51:59 +0100

    Fix revert

--- src/websiteBundle/Controller/ConversationController.php
+++ src/websiteBundle/Controller/ConversationController.php
@@ -3,21 +3,56 @@
 namespace websiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
 
 class ConversationController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
       $session = $this->container->get('session');
       $sessionStarted = $this->container->get('session')->isStarted();
       if(!$sessionStarted) {
-          $session->start();
+        $session->start();
       }
 
-      $provider = $container->get('fos_message.provider');
+      $repositoryStyxuserbase = $this->getDoctrine()->getRepository('coreBundle:WebsiteStyxuserbase');
+      $idUser = $this->getUser()->getId();
+      $user = $repositoryStyxuserbase->findById($idUser)[0];
+
+      $receiver = $repositoryStyxuserbase->findById(1)[0];
+
+      // $composer = $this->container->get('fos_message.composer');
+      // $thread = $composer->newThread();
+      // $message = $thread
+      //   ->setSender($user)
+      //   ->addRecipient($receiver)
+      //   ->setSubject('Hi there')
+      //   ->setBody('This is a test message')
+      //   ->getMessage();
+      // var_dump($message);
+
+      // exit;
+      // $thread = $message->getThread();
+
+      // $message = $composer->reply($thread)
+      //   ->setSender($receiver)
+      //   ->setBody('This is the answer to the test message')
+      //   ->getMessage();
+
+      // var_dump($thread->getSubject());
+      // $sender = $this->container->get('fos_message.sender');
+      // $sender->send($message);
+      $provider = $this->container->get('fos_message.provider');
       $threads = $provider->getInboxThreads();
-      var_dump($threads);
-      exit;
-      return $this->render('websiteBundle:conversation:conversation.html.twig');
+      foreach ($threads as $key => $value) {
+        $threads[$key]->isRead = $threads[$key]->isReadByParticipant($user);
+      }
+      // var_dump($threads);
+      // exit;
+      return $this->render('@website/conversation/conversation.html.twig', array(
+        'threads' => $threads,
+        'user' => $user
+      ));
     }
 }
